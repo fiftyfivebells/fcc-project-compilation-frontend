a0549e67898e5446d6a4a104367745a776cf6ca3
Stephen <stephen.l.bell@gmail.com>
2022-01-02 14:14:35 -0500

    Create component for rendering list of links

---
+++ src/components/layout/LinkList.tsx
@@ -0,0 +1,26 @@
+import List from "@mui/material/List";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
+import { Link } from 'react-router-dom';
+
+export default function LinkList({ links }: { links: string[] }): JSX.Element {
+
+  function linkToListItem(link: string): JSX.Element {
+    const linkText = link.toLowerCase().split(" ").join("-");
+    return (
+      <ListItemButton key={link}>
+        <Link style={{ textDecoration: "none", color: "black", }} to={link === "Home" ? "/" : `/${linkText}`}>
+          <ListItemText primary={link} />
+        </Link>
+      </ListItemButton>
+    );
+  }
+
+  const listItems: JSX.Element[] = links.map(linkToListItem);
+
+  return (
+    <List>
+      {listItems}
+    </List>
+  );
+}
