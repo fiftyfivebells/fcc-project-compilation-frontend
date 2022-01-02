bc4c39ea89e15b5b442ddc84f2f17666df235ea6
Stephen <stephen.l.bell@gmail.com>
2022-01-02 13:47:00 -0500

    Add react-router routes to root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppMenu from './components/layout/AppMenu';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+      <AppMenu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
