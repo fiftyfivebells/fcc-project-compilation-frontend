3f1ee8165b52e21a285b0f1563d1fc31661b2ede
Stephen <stephen.l.bell@gmail.com>
2022-01-04 21:12:24 -0500

    Add route for quote machine

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AppMenu from './components/layout/AppMenu';
 import Home from './components/Home';
+import QuoteGenerator from './components/QuoteGenerator';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <AppMenu />
+        <AppMenu />
         <Routes>
           <Route path="/" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/random-quote-machine" element={<QuoteGenerator />} />
         </Routes>
       </BrowserRouter>
     </>
