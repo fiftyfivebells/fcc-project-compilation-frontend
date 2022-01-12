582215d68b794bf3b0c40d865a414cd04dc94c6f
Stephen <stephen.l.bell@gmail.com>
2022-01-11 23:18:46 -0500

    Create route for MarkdownPreviewer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AppMenu from './components/layout/AppMenu';
 import Home from './components/Home';
 import QuoteGenerator from './components/QuoteGenerator';
 import Calculator from './components/calculator/Calculator';
+import MarkdownPreviewer from './components/markdown/MarkdownPreviewer';
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/random-quote-machine" element={<QuoteGenerator />} />
-          <Route path="/calculator" element={<Calculator />} />        </Routes>
+          <Route path="/calculator" element={<Calculator />} />        
+          <Route path="/markdown-previewer" element={<MarkdownPreviewer />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
