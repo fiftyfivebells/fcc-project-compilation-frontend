3a8276f6a3960f0e3e53881bdbe2666c7c3e6218
Stephen <stephen.l.bell@gmail.com>
2022-01-11 23:19:28 -0500

    Finish TextScreen implementation

--- src/components/markdown/TextScreen.tsx
+++ src/components/markdown/TextScreen.tsx
@@ -1,8 +1,41 @@
+import React from 'react';
+import { AppBar, Box, TextField, Typography } from "@mui/material";
+import TextScreenProps from "./TextScreenProps";
 
 
-export default function TextScreen({ text }: { text: string }): JSX.Element {
+
+export default function TextScreen({ props }: { props: TextScreenProps }): JSX.Element {
 
   return (
-    <></>
+    <Box
+      sx={{
+        minHeight: "80vh",
+        minWidth: "75vh",
+        border: "3px solid black"
+      }}
+    >
+      <AppBar
+        sx={{
+          display: "flex",
+          alignItems: "center"
+        }}
+        position="static"
+      >
+        <Typography variant="h4">
+          {props.title}
+        </Typography>
+      </AppBar>
+      {
+        props.html
+          ? <div style={{ padding: "15px" }} dangerouslySetInnerHTML={{ __html: props.text }}></div> 
+          : <TextField
+              variant="outlined"
+              multiline
+              fullWidth
+              value={props.text}
+              onChange={props.handleChange}
+            />
+      }
+    </Box>
   )
 }
