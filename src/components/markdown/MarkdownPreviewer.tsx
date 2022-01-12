fad5422b6b550d3b29c44b280fdb983c9d648fca
Stephen <stephen.l.bell@gmail.com>
2022-01-11 23:19:57 -0500

    Complete MarkdownPreviewer implementation

--- src/components/markdown/MarkdownPreviewer.tsx
+++ src/components/markdown/MarkdownPreviewer.tsx
@@ -1,9 +1,51 @@
-
+import { Stack } from "@mui/material";
+import React, { useState } from "react";
+import TextScreen from "./TextScreen";
+import { marked } from "marked";
+import DOMPurify from "dompurify";
 
 export default function MarkdownPreviewer(): JSX.Element {
 
+  const [markdown, setMarkdown] = useState("# Markdown Previewer");
+
+  marked.setOptions({
+    breaks: true
+  });
+  
+  const html = marked.parse(markdown);
+  const purifiedHtml = DOMPurify.sanitize(html);
+  
+  function handleTextAreaChange(e: React.ChangeEvent): void {
+    const element = e.target as HTMLInputElement;
+    const val = element.value;
+
+    setMarkdown(val);
+  }
+
+  const editorProps = {
+    title: "Editor",
+    text: markdown,
+    handleChange: handleTextAreaChange,
+    html: false
+  }
+
+  const previewProps = {
+    title: "Preview",
+    text: purifiedHtml,
+    handleChange: handleTextAreaChange,
+    html: true
+  }
+
   return (
-    <>
-    </>
+    <Stack
+      sx={{ height: "90vh" }}
+      direction="row"
+      justifyContent="space-around"
+      alignItems="center"
+      alignContent="center"
+    >
+      <TextScreen props={editorProps} />
+      <TextScreen props={previewProps} />
+    </Stack>
   )
 }
